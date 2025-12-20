bd793dde6cacbe27aa40d5928c4cdf92372b09e5
g1fttt <george.goworow2015@gmail.com>
2025-12-20 19:14:57 +0100

    feat(editor): new ui

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -8,7 +8,9 @@ import { solarizedDark } from "@fsegurai/codemirror-theme-solarized-dark"
 
 import { onMounted, ref } from "vue"
 
-const editorContainer = ref(null)
+let editorView = undefined
+
+const editAreaRef = ref(null)
 
 onMounted(() => {
   const startState = EditorState.create({
@@ -16,23 +18,65 @@ onMounted(() => {
     extensions: [basicSetup, keymap.of(defaultKeymap), javascript(), solarizedDark],
   })
 
-  new EditorView({
+  editorView = new EditorView({
     state: startState,
-    parent: editorContainer.value,
+    parent: editAreaRef.value,
   })
 })
+
+function onRunButtonClick() {
+  if (!editorView) {
+    return
+  }
+
+  // TODO: Execute code using Worker and Function
+  console.log(editorView.state.doc.toString())
+}
 </script>
 
 <template>
-  <section id="editor-container" ref="editorContainer"></section>
+  <div id="editor-container">
+    <div id="edit-area" ref="editAreaRef"></div>
+    <button @click="onRunButtonClick()" id="run-button">Run</button>
+  </div>
 </template>
 
 <style scoped>
 #editor-container {
-  overflow: auto;
+  padding: 1rem;
 
-  &:deep(.cm-editor) {
-    height: 100%;
+  & > #edit-area {
+    overflow: auto;
+    contain: inline-size;
+    height: 85vh;
+    border-style: solid;
+    border-radius: 0.5rem;
+    border-color: #00252f;
+
+    &:deep(.cm-editor) {
+      height: 100%;
+    }
+  }
+
+  & > #run-button {
+    margin-top: 1rem;
+    width: 5rem;
+    height: 2rem;
+    float: right;
+    border-style: none;
+    border-radius: 4px;
+    color: var(--sol-base03);
+    background-color: var(--sol-green);
+    filter: brightness(80%);
+    transition: 350ms;
+
+    &:hover {
+      filter: brightness(100%);
+    }
+
+    &:active {
+      background-color: var(--sol-base00);
+    }
   }
 }
 </style>
