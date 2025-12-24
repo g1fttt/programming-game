7ef17c67ceed3a8949f5f3f54c4f54193f46e5a3
g1fttt <george.goworow2015@gmail.com>
2025-12-24 20:50:25 +0100

    feat: line by line interpreter

--- src/game-worker.js
+++ src/game-worker.js
@@ -1,21 +1,53 @@
+import Interpreter from "js-interpreter"
+import * as Babel from "@babel/standalone"
+
 self.onmessage = function (ev) {
   const { code, state } = ev.data
 
   const api = {
-    movePlayer: (x, y) => {
-      ctx.state.player.pos.x = x
-      ctx.state.player.pos.y = y
-    },
+    moveEast: () => (state.player.pos.x += 1),
+    moveWest: () => (state.player.pos.x -= 1),
+    moveSouth: () => (state.player.pos.y += 1),
+    moveNorth: () => (state.player.pos.y -= 1),
   }
 
-  const ctx = { state, ...api }
+  const initFunc = (interpreter, globalObject) => {
+    const pseudoState = interpreter.nativeToPseudo(state)
+    interpreter.setProperty(globalObject, "state", pseudoState)
+
+    const createCommand = (fn) => {
+      return interpreter.createNativeFunction((...args) => {
+        fn(...args)
+
+        const updatedPseudo = interpreter.nativeToPseudo(state)
+        interpreter.setProperty(globalObject, "state", updatedPseudo)
+      })
+    }
+
+    for (const [key, value] of Object.entries(api)) {
+      interpreter.setProperty(globalObject, key, createCommand(value))
+    }
+  }
 
   try {
-    const runner = new Function(...Object.keys(ctx), code)
+    const transformedCode = Babel.transform(code, { presets: ["env"] }).code
+    const interpreter = new Interpreter(transformedCode, initFunc)
 
-    runner(...Object.values(ctx))
+    function run() {
+      const getCurrentState = (interpreter) => {
+        const pseudoState = interpreter.getValueFromScope("state")
+        return interpreter.pseudoToNative(pseudoState)
+      }
 
-    self.postMessage(state)
+      if (interpreter.step()) {
+        self.postMessage(getCurrentState(interpreter))
+
+        // TODO: Make timeout configurable
+        setTimeout(run, 25)
+      }
+    }
+
+    run()
   } catch (err) {
     console.error(err)
   }
